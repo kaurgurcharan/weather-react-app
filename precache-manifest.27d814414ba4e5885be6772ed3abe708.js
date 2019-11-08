0f1926adc0e25a23c97561a7e37fd14800380702
Gurcharan Kaur <gurcharankaur@ravjots-air.lan>
2019-11-08 15:41:54 -0500

    Updates

---
+++ precache-manifest.27d814414ba4e5885be6772ed3abe708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e3990a82b88489811ccee1f62bc266",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d67162bac71cbe51ffce",
+    "url": "/react-gh-pages/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "edb110022ea1511ebaa2",
+    "url": "/react-gh-pages/static/css/main.f63d7464.chunk.css"
+  },
+  {
+    "revision": "d67162bac71cbe51ffce",
+    "url": "/react-gh-pages/static/js/2.db3fef3d.chunk.js"
+  },
+  {
+    "revision": "edb110022ea1511ebaa2",
+    "url": "/react-gh-pages/static/js/main.4310a62b.chunk.js"
+  },
+  {
+    "revision": "ecde75a015d414b823cc",
+    "url": "/react-gh-pages/static/js/runtime~main.f344a715.js"
+  },
+  {
+    "revision": "61a0f3208643105a460fb523b219525c",
+    "url": "/react-gh-pages/static/media/backGround.61a0f320.gif"
+  }
+]);
